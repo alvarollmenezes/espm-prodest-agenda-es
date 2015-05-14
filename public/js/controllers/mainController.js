9b865433a4e4b9358bfc19979e43959747bee04b
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-05-13 21:44:01 -0300

    first version

---
+++ public/js/controllers/mainController.js
@@ -0,0 +1,8 @@
+angular.module('App').controller('mainController', ['$scope', function($scope) {
+    //$scope.eventSource = {
+    //    url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+    //    className: 'gcal-event',           // an option!
+    //    currentTimezone: 'America/Chicago' // an option!
+    //};
+    $scope.eventSource = []
+}]);
