015c618f7ad0c6d421129233f2be13bf64d9618d
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-05-13 23:24:32 -0300

    first version with angular

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,8 +1,26 @@
-angular.module('App').controller('mainController', ['$scope', function($scope) {
-    //$scope.eventSource = {
-    //    url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
-    //    className: 'gcal-event',           // an option!
-    //    currentTimezone: 'America/Chicago' // an option!
-    //};
-    $scope.eventSource = []
+angular.module('App').controller('mainController', ['$scope', function($scope,uiCalendarConfig) {
+
+    $scope.uiConfig = {
+        calendar:{
+            height: 700,
+            editable: false,
+            timezone:'America/Sao_Paulo',
+            ignoreTimezone:false,
+            header:{
+                left: 'month basicWeek basicDay agendaWeek agendaDay',
+                center: 'title',
+                right: 'today prev,next'
+            }
+
+        }
+    };
+
+
+    $scope.eventSources = {
+        googleCalendarApiKey: 'AIzaSyDQla3vMpeSZmdLQTCRiw5KaTYC7Z5aG8k',
+        url: "http://www.google.com/calendar/feeds/agendaempreendedora2015%40gmail.com/public/basic",
+        className: 'gcal-event',           // an option!
+        currentTimezone: 'America/Sao_Paulo', // an option!
+        lang: 'pt-br'
+    };
 }]);
