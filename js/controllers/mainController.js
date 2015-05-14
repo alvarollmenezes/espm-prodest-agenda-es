7028cb8bdba9a85614b841b16b475c7b5d244a6e
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-05-14 00:21:22 -0300

    change lang to pt-br

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,26 +1,32 @@
 angular.module('App').controller('mainController', ['$scope', function($scope,uiCalendarConfig) {
 
+
+
+
+
     $scope.uiConfig = {
         calendar:{
-            height: 700,
-            editable: false,
-            timezone:'America/Sao_Paulo',
-            ignoreTimezone:false,
+            dayNames : ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            dayNamesShort : ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+            monthNames : "Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro".split("_"),
+            monthNamesShort : "jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez".split("_"),
+            height: 450,
+            editable: true,
             header:{
-                left: 'month basicWeek basicDay agendaWeek agendaDay',
-                center: 'title',
+                left: 'month basicWeek basicDay',
+                center: '',
                 right: 'today prev,next'
             }
-
         }
     };
 
 
-    $scope.eventSources = {
+
+
+
+    $scope.eventSources = [{
         googleCalendarApiKey: 'AIzaSyDQla3vMpeSZmdLQTCRiw5KaTYC7Z5aG8k',
         url: "http://www.google.com/calendar/feeds/agendaempreendedora2015%40gmail.com/public/basic",
-        className: 'gcal-event',           // an option!
-        currentTimezone: 'America/Sao_Paulo', // an option!
-        lang: 'pt-br'
-    };
+        currentTimezone: 'America/Sao_Paulo' // an option!
+    }];
 }]);
