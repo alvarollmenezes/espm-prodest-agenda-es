a2b25c2da3b05d730700e7141821c2ac63c23f95
Daniel Hoisel Ferraz <danielhoiselferraz@gmail.com>
2016-04-10 12:16:23 -0300

    removendo arquivos

--- jspm_packages/github/angular-ui/ui-calendar@1.0.1.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("github:angular-ui/ui-calendar@1.0.1/src/calendar.js");
--- jspm_packages/github/fullcalendar/fullcalendar@2.6.1.js
+++
@@ -1,3 +0,0 @@
-define(["github:fullcalendar/fullcalendar@2.6.1/fullcalendar"], function(main) {
-  return main;
-});
